883569c88ceb2c1486e4ebe103f6ef771a2085c8
JesseKoldewijn <jesse.koldewijn@denieuwezaak.nl>
2024-07-12 13:23:33 +0200

    feat: moved over to shadcn tooltip instead of custom

--- src/components/auth/user-badge.tsx
+++ src/components/auth/user-badge.tsx
@@ -1,59 +1,61 @@
 "use client";
 
 import { useAuth } from "@/providers/AuthProvider";
+
+import {
+  TooltipProvider,
+  Tooltip,
+  TooltipTrigger,
+  TooltipContent,
+} from "../ui/tooltip";
 import { useRouter } from "next/navigation";
-import { useState } from "react";
 
 const UserBadge = () => {
   const { user, clientIP } = useAuth();
-  const [hovering, isHovering] = useState(false);
-
   const router = useRouter();
 
   return (
-    <div
-      className="fixed bottom-6 right-6 flex h-14 w-14 cursor-pointer items-center justify-center rounded-full border-2 border-foreground bg-foreground text-3xl text-background transition-colors duration-500"
-      onMouseEnter={() => isHovering(true)}
-      onMouseLeave={() => isHovering(false)}
-      onClick={(e) => {
-        e.preventDefault();
-        const isMobile = window.innerWidth < 768;
-        if (user?.username) {
-          if (isMobile) {
-            isHovering(!hovering);
-          }
-        } else {
-          router.push("/login");
-        }
-      }}
-    >
-      {user?.username ? (
-        <Initials username={user.username} />
-      ) : (
-        <Initials username={"?"} />
-      )}
-      <div
-        style={{ display: hovering ? "block" : "none" }}
-        className="fixed bottom-24 right-6 w-max rounded-lg bg-foreground px-3 py-2 text-sm text-background shadow-lg"
-      >
-        {user?.username ? (
-          <>
-            <div className="flex gap-2">
-              <strong className="w-20 whitespace-nowrap">Username</strong>
-              <span className="ml-auto w-full text-right">{user.username}</span>
-            </div>
-            <div className="flex gap-2">
-              <strong className="w-20 whitespace-nowrap">Client IP</strong>
-              <span className="ml-auto w-full text-right">{clientIP}</span>
-            </div>
-          </>
-        ) : (
-          <>
-            <div className="flex gap-2">Feel free to login!</div>
-          </>
-        )}
-      </div>
-    </div>
+    <TooltipProvider>
+      <Tooltip>
+        <TooltipTrigger className="fixed bottom-4 right-3 flex h-10 w-10 cursor-pointer items-center justify-center rounded-full border-2 border-foreground bg-foreground text-2xl text-background transition-colors duration-500 sm:bottom-5 sm:right-4 sm:h-14 sm:w-14 sm:text-3xl">
+          <span
+            className="inset-0"
+            onClick={(e) => {
+              if (!user?.username) {
+                e.preventDefault();
+                router.push("/login");
+              }
+            }}
+          >
+            {user?.username ? (
+              <Initials username={user.username} />
+            ) : (
+              <Initials username="?" />
+            )}
+          </span>
+        </TooltipTrigger>
+        <TooltipContent className="mb-4 mr-4 w-max rounded-lg bg-foreground px-3 py-2 text-sm text-background shadow-lg">
+          {user?.username ? (
+            <>
+              <div className="flex gap-2">
+                <strong className="w-20 whitespace-nowrap">Username</strong>
+                <span className="ml-auto w-full text-right">
+                  {user.username}
+                </span>
+              </div>
+              <div className="flex gap-2">
+                <strong className="w-20 whitespace-nowrap">Client IP</strong>
+                <span className="ml-auto w-full text-right">{clientIP}</span>
+              </div>
+            </>
+          ) : (
+            <>
+              <div className="flex gap-2">Feel free to login!</div>
+            </>
+          )}
+        </TooltipContent>
+      </Tooltip>
+    </TooltipProvider>
   );
 };
 
